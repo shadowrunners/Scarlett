826d45153b8b1937da91984c503bf84c6b053d42
scrappie <notscrappie@proton.me>
2023-10-10 22:34:42 +0300

    refactor: export all classes

---
+++ src/Models/index.ts
@@ -0,0 +1,5 @@
+import { Album } from "./Album";
+import { Track } from "./Track";
+import { Playlist } from "./Playlist";
+
+export { Album, Track, Playlist };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import { Manager } from "./Manager.js";
+import { Player } from "./Player.js";
+import { 
+    Album,
+    Track,
+    Playlist
+} from "./Models/index.js";
+import Queue from "./Queue.js";
 
-export { Manager };
+export { 
+    Manager,
+    Player,
+    Track,
+    Queue,
+    Album,
+    Playlist,
+};
