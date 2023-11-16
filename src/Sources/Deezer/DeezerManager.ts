9a158106fe90f3508099557f1e47f5eba36f2f62
scrappie <notscrappie@proton.me>
2023-11-16 17:35:22 +0200

    refactor(sources): builder changes + eslint

--- src/Sources/Deezer/DeezerManager.ts
+++ src/Sources/Deezer/DeezerManager.ts
@@ -1,85 +1,81 @@
+import { ResolveResponse, ResultTypes } from '../../Manager.js';
+import { DeezerBuilder } from '../../Utils/Builders';
 import axios from 'axios';
-import { Builder } from '../../Utils/Builder';
-import { ResolveResponse, ResultTypes } from '../../Manager';
 
 export class Deezer {
-    /** The public API's URL. */
-    private publicAPI: string;
-    /** The Builder class used to build track data. */
-    private builder: Builder;
-    /** The regex for standard Deezer links. */
-    private deezerRegex: RegExp;
-    /** The regex for page.link links. */
-    private pageLinkRegex: RegExp;
+	/** The public APIs URL. */
+	private readonly publicAPI: string = 'https://api.deezer.com';
+	/** The Builder class used to build track data. */
+	private builder: DeezerBuilder;
+	/** The regex for standard Deezer links. */
+	private readonly deezerRegex: RegExp = /^(https?:\/\/)?deezer\.com\/(?<countrycode>[a-zA-Z]{2}\/)?(?<type>track|album|playlist|artist)\/(?<identifier>[0-9]+)/;
+	/** The regex for page.link links. */
+	private readonly pageLinkRegex: RegExp = /^(https?:\/\/)?deezer\.page\.link\/[a-zA-Z0-9]+$/;
 
-    constructor() {
-        this.publicAPI = 'https://api.deezer.com';
-        this.builder = new Builder();
+	constructor() {
+		this.builder = new DeezerBuilder();
+	}
 
-        this.deezerRegex = /^(https?:\/\/)?deezer\.com\/(?<countrycode>[a-zA-Z]{2}\/)?(?<type>track|album|playlist|artist)\/(?<identifier>[0-9]+)/;
-        this.pageLinkRegex = /^(https?:\/\/)?deezer\.page\.link\/[a-zA-Z0-9]+$/;
-    }
-
-    /** 
+	/**
      * Fetches the query based on the regex.
      * @param query The link of the track / album / playlist or the query.
      * @returns The appropriate response.
      */
-    public async resolve(query: string): Promise<ResolveResponse> {
-        let songQuery: string;
+	public async resolve(query: string): Promise<ResolveResponse> {
+		let songQuery: string;
 
-        if (query.match(this.pageLinkRegex)) songQuery = await this.resolveShareUrl(query);
-        else songQuery = query;
+		if (query.match(this.pageLinkRegex)) songQuery = await this.resolveShareUrl(query);
+		else songQuery = query;
 
-        const identifier = songQuery.match(this.deezerRegex) || null;
-        switch (identifier?.groups?.type) {
-            case 'album':
-                return {
-                    type: ResultTypes.ALBUM,
-                    info: await this.fetchAlbum(identifier.groups.identifier),
-                };
-            case 'track':
-                return {
-                    type: ResultTypes.TRACK,
-                    info: await this.fetchSong(identifier.groups.identifier),
-                };
-            case 'playlist': 
-                return {
-                    type: ResultTypes.PLAYLIST,
-                    info: await this.fetchPlaylist(identifier.groups.identifier)
-                }
-            default:
-                return {
-                    type: ResultTypes.SEARCH,
-                    info: await this.fetchQuery(query),
-                }
-        }
-    }
+		const identifier = songQuery.match(this.deezerRegex) || null;
+		switch (identifier?.groups?.type) {
+		case 'album':
+			return {
+				type: ResultTypes.ALBUM,
+				info: await this.fetchAlbum(identifier.groups.identifier),
+			};
+		case 'track':
+			return {
+				type: ResultTypes.TRACK,
+				info: await this.fetchSong(identifier.groups.identifier),
+			};
+		case 'playlist':
+			return {
+				type: ResultTypes.PLAYLIST,
+				info: await this.fetchPlaylist(identifier.groups.identifier),
+			};
+		default:
+			return {
+				type: ResultTypes.SEARCH,
+				info: await this.fetchQuery(query),
+			};
+		}
+	}
 
-    private async fetchQuery(query: string) {
-        const res = await axios.get(`${this.publicAPI}/search?q=${encodeURIComponent(query)}`);
-        return this.builder.buildDeezerTrack((res.data as QueryResponse).data[0]);
-    }
+	private async fetchQuery(query: string) {
+		const res = await axios.get(`${this.publicAPI}/search?q=${encodeURIComponent(query)}`);
+		return this.builder.buildTrack((res.data as QueryResponse).data[0]);
+	}
 
-    private async fetchSong(query: string) {
-        const res = await axios.get(`${this.publicAPI}/track/${query}`);
-        return this.builder.buildDeezerTrack(res.data as APITrackResponse);
-    }
+	private async fetchSong(query: string) {
+		const res = await axios.get(`${this.publicAPI}/track/${query}`);
+		return this.builder.buildTrack(res.data as APITrackResponse);
+	}
 
-    private async fetchAlbum(query: string) {
-        const res = await axios.get(`${this.publicAPI}/album/${query}`);
-        return this.builder.buildDeezerAlbum(res.data as APIAlbum);
-    }
+	private async fetchAlbum(query: string) {
+		const res = await axios.get(`${this.publicAPI}/album/${query}`);
+		return this.builder.buildAlbum(res.data as APIAlbum);
+	}
 
-    private async fetchPlaylist(query: string) {
-        const res = await axios.get(`${this.publicAPI}/playlist/${query}`);
-        return this.builder.buildDeezerPlaylist(res.data as APIPlaylist);
-    }
+	private async fetchPlaylist(query: string) {
+		const res = await axios.get(`${this.publicAPI}/playlist/${query}`);
+		return this.builder.buildPlaylist(res.data as APIPlaylist);
+	}
 
-    private async resolveShareUrl(query: string) {
-        const res = await axios.get(query);
-        return `https://deezer.com${res.request.path}`;
-    }
+	private async resolveShareUrl(query: string) {
+		const res = await axios.get(query);
+		return `https://deezer.com${res.request.path}`;
+	}
 }
 
 interface QueryResponse {
@@ -111,7 +107,6 @@ interface APIAlbum {
     title: string;
     cover_big: string;
     link: string;
-    label: string;
     tracks: {
         data: APITrackResponse[];
     };
@@ -119,6 +114,7 @@ interface APIAlbum {
     artist: {
         name: string;
     };
+    upc: string;
 }
 
 interface APIPlaylist {
--- src/Sources/SoundCloud/SoundCloudManager.ts
+++ src/Sources/SoundCloud/SoundCloudManager.ts
@@ -1,92 +1,112 @@
-import { ResolveResponse, ResultTypes } from "../../Manager";
-import { Builder } from "../../Utils/Builder";
-import axios from "axios";
+import { Manager, ResolveResponse, ResultTypes } from '../../Manager.js';
+import { SoundCloudBuilder } from '../../Utils/Builders';
+import axios from 'axios';
 
 /** This is Disrupt's SoundCloud source manager. Used for resolving SoundCloud links. */
 export class SoundCloud {
-    /** The client ID used to call the API. */
-    private clientId: string;
-    /** The URL of the API. */
-    private apiURL: string;
-    /** The builder class used to build track and album metadata. */
-    private builder: Builder;
-    /** The regex used to detect SoundCloud links. */
-    private scRegex: RegExp;
-    
-    constructor() {
-        this.apiURL = 'https://api-v2.soundcloud.com';
-        this.builder = new Builder();
+	/** The client ID used to call the API. */
+	private readonly clientId: string;
+	/** The URL of the API. */
+	private readonly apiURL: string;
+	/** The builder class used to build track and album metadata. */
+	private builder: SoundCloudBuilder;
+	/** The regex used to detect SoundCloud links. */
+	private readonly scRegex: RegExp;
 
-        this.scRegex = /^(https?:\/\/)?(?:www\.)?soundcloud\.com\/([^/]+)\/(sets\/[^/]+|[^/]+)\/?$/;
-    }
+	constructor(disrupt: Manager) {
+		this.apiURL = 'https://api-v2.soundcloud.com';
+		this.builder = new SoundCloudBuilder();
+		this.clientId = disrupt.options.sources.soundcloud.clientId;
 
-    /**
+		this.scRegex = /^(https?:\/\/)?(?:www\.)?soundcloud\.com\/([^/]+)\/(sets\/[^/]+|[^/]+)\/?$/;
+	}
+
+	/**
      * Fetches the query based on the regex.
      * @param query The link of the track / album / playlist or the query.
      * @returns The appropriate response.
      */
-    public async resolve(query: string): Promise<ResolveResponse> {
-        const identifier = query.match(this.scRegex) || null;
-        console.log(identifier)
+	public async resolve(query: string): Promise<ResolveResponse> {
+		const identifier = query.match(this.scRegex) || null;
+		console.log(identifier);
 
-        if (!identifier) return {
-            type: ResultTypes.SEARCH,
-            info: await this.fetchQuery(query),
-        }
+		if (!identifier) return {
+			type: ResultTypes.SEARCH,
+			info: await this.fetchQuery(query),
+		};
 
-        if (identifier[3] && identifier[3].startsWith('sets')) return {
-            type: ResultTypes.ALBUM,
-            info: await this.fetchPlaylist(query),
-        }
+		if (identifier[3] && identifier[3].startsWith('sets')) return {
+			type: ResultTypes.ALBUM,
+			info: await this.fetchPlaylist(query),
+		};
 
-        return {
-            type: ResultTypes.TRACK,
-            info: await this.fetchSong(query),
-        }
-    }
+		return {
+			type: ResultTypes.TRACK,
+			info: await this.fetchSong(query),
+		};
+	}
 
-    private async fetchQuery(query: string) {
-        const res = await axios.get(`${this.apiURL}/search/tracks?q=${query}&client_id=${this.clientId}`);
-        const jsonResponse = res.data as SearchResult;
+	private async fetchQuery(query: string) {
+		const res = await axios.get(`${this.apiURL}/search/tracks?q=${query}&client_id=${this.clientId}`);
+		const jsonResponse = res.data as SearchResult;
 
-        return this.builder.buildSCTrack(jsonResponse.collection[0]);
-    }
+		return this.builder.buildTrack(jsonResponse.collection[0]);
+	}
 
-    private async fetchSong(query: string) {
-        const res = await axios.get(`${this.apiURL}/resolve?url=${query}&client_id=${this.clientId}`);
-        const jsonResponse = res.data as APIResponse;
+	private async fetchSong(query: string) {
+		const res = await axios.get(`${this.apiURL}/resolve?url=${query}&client_id=${this.clientId}`);
+		const jsonResponse = res.data as APIResponse;
 
-        return this.builder.buildSCTrack(jsonResponse);
-    }
+		return this.builder.buildTrack(jsonResponse);
+	}
 
-    private async fetchPlaylist(query: string) {
-        const res = await axios.get(`${this.apiURL}/resolve?url=${query}&client_id=${this.clientId}`);
-        const jsonResponse = res.data as APIResponse;
+	private async fetchPlaylist(query: string) {
+		const res = await axios.get(`${this.apiURL}/resolve?url=${query}&client_id=${this.clientId}`);
+		const jsonResponse = res.data as APIPlaylist;
 
-        return this.builder.buildSCPlaylist(jsonResponse);
-    }
+		// Fetches all the songs in the playlist since sometimes SC only provides IDs.
+		const tData = await axios.get(`${this.apiURL}/tracks?ids=${jsonResponse.tracks.map((track) => `${track.id},`)}`);
+
+		return this.builder.buildPlaylist(jsonResponse, tData.data as APIResponse[]);
+	}
 }
 
 interface SearchResult {
     collection: APIResponse[];
 }
 
-interface APIResponse {
+interface APIPlaylist {
+	id: string;
+	title: string;
+	permalink_url: string;
+	username: string;
+	duration: number;
+	artwork_url: string;
+	tracks: {
+		id: string;
+	}[];
+}
+
+type APIResponse = {
     id: string;
     artwork_url: string;
     title: string;
-    permalink: string;
+    permalink_url: string;
     duration: number;
+	publisher_metadata: {
+		artist: string;
+		isrc: string;
+	}
     media: {
         transcodings: [
             {
-               url: string; 
+               url: string;
             },
             {
-                url: string; 
+                url: string;
             },
             {
-                url: string; 
+                url: string;
             },
         ]
     }
