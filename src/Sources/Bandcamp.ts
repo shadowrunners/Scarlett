4bb1cfb654fdb6e56e2e4210096f2f430172bab5
scrappie <notscrappie@proton.me>
2023-12-01 23:15:08 +0200

    refactor: http artist now returns Unknown Artist + eslint stuff

--- src/Sources/Bandcamp.ts
+++ src/Sources/Bandcamp.ts
@@ -1,67 +1,66 @@
-import { ResolveResponse, ResultTypes } from "../Manager";
-import { Album, Track } from "../Models";
-import axios from "axios";
+import { ResolveResponse, ResultTypes } from '../Manager';
+import { Album, Track } from '../Models';
+import axios from 'axios';
 
 /** This is Disrupt's Bandcamp source manager. Used for resolving Bandcamp links. */
 export class Bandcamp {
-    public async resolve(query: string, requester: unknown): Promise<ResolveResponse> {
-        const bandData = await axios.get(query);
-        const data = /<script type="application\/ld\+json">([\s\S]*?)<\/script>/.exec(bandData.data);
-        const matches = JSON.parse(data[1]) as BandcampResponse;
+	public async resolve(query: string, requester: unknown): Promise<ResolveResponse> {
+		const bandData = await axios.get(query);
+		const data = /<script type="application\/ld\+json">([\s\S]*?)<\/script>/.exec(bandData.data);
+		const matches = JSON.parse(data[1]) as BandcampResponse;
 
-        if (!matches) return { type: ResultTypes.EMPTY, info: {} };
+		if (!matches) return { type: ResultTypes.EMPTY, info: {} };
 
-        switch (matches["@type"]) {
-            case 'MusicAlbum':
-                return {
-                    type: ResultTypes.ALBUM,
-                    info: new Album({
-                        artist: matches.byArtist.name,
-                        artworkUrl: matches.image,
-                        duration: 0,
-                        id: matches["id"],
-                        source: "bandcamp",
-                        title: matches.name,
-                        tracks: matches.track.itemListElement.map((track) => this.buildTracks(matches, track.item, requester)),
-                        uri: query,
-                        requester,
-                    }),
-                };
-            case 'MusicRecording':
-                return {
-                    type: ResultTypes.TRACK,
-                    info: new Track({
-                        artist: matches.byArtist.name,
-                        artworkUrl: matches.image,
-                        duration: 0,
-                        id: matches["id"],
-                        source: "bandcamp",
-                        title: matches.name,
-                        uri: query,
-                        requester,
-                    }),
-                };
-        }
-    }
+		switch (matches['@type']) {
+		case 'MusicAlbum':
+			return {
+				type: ResultTypes.ALBUM,
+				info: new Album({
+					artist: matches.byArtist.name,
+					artworkUrl: matches.image,
+					duration: 0,
+					id: matches['id'],
+					source: 'bandcamp',
+					title: matches.name,
+					tracks: matches.track.itemListElement.map((track) => this.buildTracks(matches, track.item, requester)),
+					uri: query,
+				}),
+			};
+		case 'MusicRecording':
+			return {
+				type: ResultTypes.TRACK,
+				info: new Track({
+					artist: matches.byArtist.name,
+					artworkUrl: matches.image,
+					duration: 0,
+					id: matches['id'],
+					source: 'bandcamp',
+					title: matches.name,
+					uri: query,
+					requester,
+				}),
+			};
+		}
+	}
 
-    /**
+	/**
      * Builds the Bandcamp tracks.
      * @param data The raw data about the album.
      * @param trackData The raw data about the array of tracks.
      * @param requester The person who requested the album (gets appended to all tracks).
      */
-    private buildTracks(data: BandcampResponse, trackData: AlbumTrack, requester: unknown) {
-        return new Track({
-            artist: data.byArtist.name,
-            artworkUrl: data.image,
-            duration: 0,
-            id: trackData["@id"],
-            requester,
-            source: "bandcamp",
-            title: trackData.name,
-            uri: trackData["@id"],
-        })
-    }
+	private buildTracks(data: BandcampResponse, trackData: AlbumTrack, requester: unknown) {
+		return new Track({
+			artist: data.byArtist.name,
+			artworkUrl: data.image,
+			duration: 0,
+			id: trackData['@id'],
+			requester,
+			source: 'bandcamp',
+			title: trackData.name,
+			uri: trackData['@id'],
+		});
+	}
 }
 
 type BandcampResponse = {
--- src/Sources/HTTP.ts
+++ src/Sources/HTTP.ts
@@ -6,17 +6,13 @@ import axios from 'axios';
 export class HTTP {
 	public async resolve(query: string, requester: unknown): Promise<ResolveResponse> {
 		// Source is mostly going to be used for radio stations, therefore, fetch the ICECAST header data.
-		// This only handles AAC encoded playlist streams. No M3U8 stuff yet.
-
-		// TODO: Implement M3U8 support. (probably via query.contains('m3u8') -> use m3u8stream)
-
 		const res = await axios.get(query, { responseType: 'stream' });
 
 		return {
 			type: ResultTypes.TRACK,
 			info: new Track({
 				id: undefined,
-				artist: undefined,
+				artist: 'Unknown Artist',
 				artworkUrl: undefined,
 				duration: 0,
 				requester,
