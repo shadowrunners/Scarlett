e69129d247d59f2185baef7edbe8914264c28da2
scrappie <notscrappie@proton.me>
2023-12-16 18:00:01 +0200

    refactor(builders): removed deezer and tidal builder
    from the file

--- src/Utils/Builders/index.ts
+++ src/Utils/Builders/index.ts
@@ -1,7 +1,5 @@
-import { DeezerBuilder } from './DeezerBuilder.js';
 import { SpotifyBuilder } from './SpotifyBuilder.js';
-// import { TIDALBuilder } from './TIDALBuilder.js';
 import { SoundCloudBuilder } from './SoundCloudBuilder.js';
 
-// export { DeezerBuilder, SpotifyBuilder, TIDALBuilder, SoundCloudBuilder };
-export { DeezerBuilder, SpotifyBuilder, SoundCloudBuilder };
+// export { SpotifyBuilder, SoundCloudBuilder };
+export { SpotifyBuilder, SoundCloudBuilder };
